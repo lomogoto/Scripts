3d9e6ef595b2472d20868a7c471f35100ff7093d
lomogoto <brad.victor.tucker@gmail.com>
2014-04-03 23:22:10 +0000

    linking file added as readme

---
+++ README.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# RREAD ME
+# this file can be executed to place files in the correct places
+
+ln -s aur /usr/local/bin/
+
+ln -s itav /usr/local/bin/
+
+ln -s tmuxstat /usr/local/bin/
