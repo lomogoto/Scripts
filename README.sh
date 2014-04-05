5c8c667c6e0b4e9e153f2a76298c7bd317cbeed5
Brad Tucker <brad.victor.tucker@gmail.com>
2014-04-05 11:45:39 -0700

    fix su in readme

--- README.sh
+++ README.sh
@@ -3,11 +3,10 @@
 # RREAD ME
 # this file can be executed to place files in the correct places
 #
+su -c "cp aur /usr/local/bin/ &&
 
-cp aur /usr/local/bin/
+cp itav /usr/local/bin/ &&
 
-cp itav /usr/local/bin/
+cp tmuxstat /usr/local/bin/ &&
 
-cp tmuxstat /usr/local/bin/
-
-cp git-update /usr/local/bin/
+cp git-update /usr/local/bin/ "
