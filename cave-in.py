b9279e05480bb462b5cb97846612abd1f5d13b30
Brad Tucker <brad.victor.tucker@gmail.com>
2014-04-09 17:40:46 -0700

    add color to display of cave-in

--- cave-in.py
+++ cave-in.py
@@ -13,10 +13,13 @@ s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 address=('',0)
 
+server = True
+
 if raw_input('\nServer?(Y/n) ')!='n':
 	print "\nYour IP address:", yourIP
 	s.bind(('', port))
 	address=s.recvfrom(1024)[1]
+	server = False
 
 else:
 	address=(raw_input("\nOpponent's IP address: " ),port)
@@ -34,6 +37,14 @@ menuPos=0
 running = True
 
 def main(screen):
+
+	curses.curs_set(0)
+	curses.init_pair(1,7,0)
+	curses.init_pair(2+server,1,0)
+	curses.init_pair(3-server,6,0)
+	curses.init_pair(4,7,7)
+	curses.init_pair(5,4,0)
+
 	global running
 	global menuPos
 
@@ -70,14 +81,22 @@ def makeMap():
 	for z in range(10):
 		level=[]
 		for y in range(45):
-			row=''
+			row=[]
 			for x in range(60):
 				char='?'
-				row+=char
+				row.append(char)
 			level.append(row)
 		gameMap.append(level)
+	gameMap[pos[0]-1][pos[1]-2][pos[2]]='f'
+	gameMap[pos[0]-1][pos[1]-1][pos[2]]='F'
+	gameMap[pos[0]-1][pos[1]][pos[2]]='X'
+	gameMap[pos[0]-1][pos[1]+1][pos[2]]='~'
 
 def printMap(screen):
+
+	enemies='fgsbt-i'
+	friends='FGSBT_!'
+
 	screen.addstr(0,18,'Cave  In')
 	screen.addstr(menuPos+3,26,' ')
 	screen.addstr(menuPos+4,26,'>')
@@ -105,14 +124,38 @@ def printMap(screen):
 		screen.addstr(6,19,'SKY')
 	else:
 		for i in range(5):
-			screen.addstr(i+4,17,gameMap[pos[0]+1][pos[1]-2+i][pos[2]-3:pos[2]+4])
+			for j in range(7):
+				char=gameMap[pos[0]+1][pos[1]-2+i][pos[2]-3+j]
+				try:
+					displaychar=friends[enemies.index(char)]
+				except:
+					displaychar=char
+				screen.addstr(i+4, 17+j ,displaychar ,curses.color_pair(1 + (enemies.find(char)!=-1) + 2*(friends.find(char)!=-1)+3*(char=='X')+4*(char=='~')))
+
 	if pos[0]==0:
 		for i in range(5):
 			screen.addstr(i+10,17,' '*7)
 		screen.addstr(12,17,'BEDROCK')
 	else:
 		for i in range(5):
-			screen.addstr(i+10,17,gameMap[pos[0]-1][pos[1]-2+i][pos[2]-3:pos[2]+4])
+			for j in range(7):
+				char=gameMap[pos[0]-1][pos[1]-2+i][pos[2]-3+j]
+				try:
+					displaychar=friends[enemies.index(char)]
+				except:
+					displaychar=char
+				screen.addstr(i+10, 17+j ,displaychar ,curses.color_pair(1 + (enemies.find(char)!=-1) + 2*(friends.find(char)!=-1)+3*(char=='X')+4*(char=='~')))
+
+	for i in range(11):
+		for j in range(15):
+			char=gameMap[pos[0]][pos[1]-5+i][pos[2]-7+j]
+			try:
+				displaychar=friends[enemies.index(char)]
+			except:
+				displaychar=char
+			screen.addstr(i+4, 1+j ,displaychar ,curses.color_pair(1 + (enemies.find(char)!=-1) + 2*(friends.find(char)!=-1)+3*(char=='X')+4*(char=='~')))
+
+
 
 def displayNum(n):
 	if n<10:
